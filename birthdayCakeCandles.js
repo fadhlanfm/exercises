e04d5a61c7328f995794196f241833e0c8e745bc
fadhlanfm <fafadhlanriz@gmail.com>
2020-02-11 18:49:33 +0700

    first commit

---
+++ birthdayCakeCandles.js
@@ -0,0 +1,16 @@
+function birthdayCakeCandles(ar) {
+    let max = ar[0]
+    for(let i=1;i<ar.length;i++){
+        if(max<ar[i]){
+            max=ar[i]
+        }
+    }
+
+    let counter = 0
+    for(let i=0;i<ar.length;i++){
+        if(max===ar[i]){
+            counter++
+        }
+    }
+    return counter
+}
